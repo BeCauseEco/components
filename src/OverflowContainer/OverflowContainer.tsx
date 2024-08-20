c39a79ca780be829113bc95f535bbb948f7f76ec
Thomas Angelbo Christensen <thomas@eject.io>
2024-08-20 16:15:13 +0200

    Update OverflowContainer.tsx

--- src/OverflowContainer/OverflowContainer.tsx
+++ src/OverflowContainer/OverflowContainer.tsx
@@ -2,12 +2,12 @@ import styled from "@emotion/styled"
 
 import { PropsWithChildren, ReactElement } from "react"
 
-const Container = styled.div({
+const Container = styled.div<Pick<TOverflowContainer, "maxHeight">>(p => ({
   display: "flex",
   flexDirection: "inherit",
   width: "inherit",
   height: "inherit",
-  outline: "solid 1px cyan",
+  ...(p.maxHeight && { maxHeight: p.maxHeight }),
 
   overflowY: "scroll",
 
@@ -29,12 +29,13 @@ const Container = styled.div({
     borderRadius: "6px",
     border: "3px solid rgba(0, 0, 0, 0.1)",
   },
-})
+}))
 
 export type TOverflowContainer = {
   children: ReactElement | ReactElement[]
+  maxHeight?: string
 }
 
-export const OverflowContainer = ({ children }: PropsWithChildren<TOverflowContainer>) => (
-  <Container>{children}</Container>
+export const OverflowContainer = ({ children, maxHeight }: PropsWithChildren<TOverflowContainer>) => (
+  <Container maxHeight={maxHeight}>{children}</Container>
 )
