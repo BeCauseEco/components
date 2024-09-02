56374a0709b598c0befee05ab42ea58ceff28f15
Thomas Angelbo Christensen <thomas@eject.io>
2024-09-02 11:31:15 +0200

    Patch/continuous (#33)

--- src/OverflowContainer/OverflowContainer.tsx
+++ src/OverflowContainer/OverflowContainer.tsx
@@ -2,20 +2,24 @@ import styled from "@emotion/styled"
 
 import { PropsWithChildren, ReactElement } from "react"
 
-const Container = styled.div<Pick<TOverflowContainer, "maxHeight">>(p => ({
+const Container = styled.div<Pick<TOverflowContainer, "maxHeight" | "omitPadding">>(p => ({
   display: "flex",
   flexDirection: "inherit",
   width: "100%",
   height: "inherit",
+  padding: p.omitPadding ? 0 : "calc(var(--BU) * 4)",
   ...(p.maxHeight && { maxHeight: p.maxHeight }),
   overflowY: "scroll",
 }))
 
 export type TOverflowContainer = {
   children: ReactElement | ReactElement[]
+  omitPadding?: boolean
   maxHeight?: string
 }
 
-export const OverflowContainer = ({ children, maxHeight }: PropsWithChildren<TOverflowContainer>) => (
-  <Container maxHeight={maxHeight}>{children}</Container>
+export const OverflowContainer = ({ children, omitPadding, maxHeight }: PropsWithChildren<TOverflowContainer>) => (
+  <Container maxHeight={maxHeight} omitPadding={omitPadding}>
+    {children}
+  </Container>
 )
