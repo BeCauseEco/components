0b6f784526145a069fb04a9d26a56f640f82318a
Thomas Angelbo Christensen <thomas@eject.io>
2024-09-02 11:35:30 +0200

    Patch/continuous (#34)

--- src/OverflowContainer/OverflowContainer.tsx
+++ src/OverflowContainer/OverflowContainer.tsx
@@ -9,7 +9,7 @@ const Container = styled.div<Pick<TOverflowContainer, "maxHeight" | "omitPadding
   height: "inherit",
   padding: p.omitPadding ? 0 : "calc(var(--BU) * 4)",
   ...(p.maxHeight && { maxHeight: p.maxHeight }),
-  overflowY: "scroll",
+  overflowY: "auto",
 }))
 
 export type TOverflowContainer = {
