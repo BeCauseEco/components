2fa4e82daa94e211ad2e93e196f22a54add418a9
Thomas Angelbo Christensen <thomas@eject.io>
2024-09-10 14:58:25 +0200

    wip

--- src/OverflowContainer/OverflowContainer.tsx
+++ src/OverflowContainer/OverflowContainer.tsx
@@ -2,12 +2,13 @@ import styled from "@emotion/styled"
 
 import { PropsWithChildren, ReactElement } from "react"
 
-const Container = styled.div<Pick<TOverflowContainer, "maxHeight" | "omitPadding" | "axes">>(p => ({
+const Container = styled.div<Pick<TOverflowContainer, "minHeight" | "maxHeight" | "omitPadding" | "axes">>(p => ({
   display: "flex",
   flexDirection: "inherit",
   width: "100%",
   height: "inherit",
   padding: p.omitPadding ? 0 : "calc(var(--BU) * 4)",
+  ...(p.minHeight && { minHeight: p.minHeight }),
   ...(p.maxHeight && { maxHeight: p.maxHeight }),
   overflowX: p.axes === EOverflowContainerAxis.Both || p.axes === EOverflowContainerAxis.XAxis ? "auto" : "hidden",
   overflowY: p.axes === EOverflowContainerAxis.Both || p.axes === EOverflowContainerAxis.YAxis ? "auto" : "hidden",
@@ -22,6 +23,7 @@ export enum EOverflowContainerAxis {
 export type TOverflowContainer = {
   axes: EOverflowContainerAxis
   omitPadding?: boolean
+  minHeight?: string
   maxHeight?: string
   children: ReactElement | ReactElement[]
 }
@@ -29,10 +31,11 @@ export type TOverflowContainer = {
 export const OverflowContainer = ({
   axes,
   omitPadding,
+  minHeight,
   maxHeight,
   children,
 }: PropsWithChildren<TOverflowContainer>) => (
-  <Container maxHeight={maxHeight} omitPadding={omitPadding} axes={axes}>
+  <Container minHeight={minHeight} maxHeight={maxHeight} omitPadding={omitPadding} axes={axes}>
     {children}
   </Container>
 )
