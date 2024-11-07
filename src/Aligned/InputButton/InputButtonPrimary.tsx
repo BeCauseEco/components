387271e2678a0bcb5b728aaaa3eea3c2a64130c8
Thomas Angelbo Christensen <thomas@eject.io>
2024-11-07 11:58:50 +0100

    Update InputButtonPrimary.tsx

--- src/Aligned/InputButton/InputButtonPrimary.tsx
+++ src/Aligned/InputButton/InputButtonPrimary.tsx
@@ -2,27 +2,36 @@ import { forwardRef } from "react"
 import { TInputButton, InputButton } from "@new/Aligned/InputButton/internal/InputButton"
 import { EColor } from "@new/Color"
 
-type TInputButtonPrimaryBase = Pick<TInputButton, "size" | "hug" | "loading" | "disabled"> & {
+type TInputButtonPrimary = Pick<TInputButton, "size" | "hug" | "loading" | "disabled"> & {
   label: string
+  iconLeftName?: string
+  iconRightName?: string
 }
 
-type TInputButtonPrimary =
-  | (TInputButtonPrimaryBase & {
-      iconLeftName?: string
-    })
-  | (TInputButtonPrimaryBase & {
-      iconRightName?: string
-    })
+export const InputButtonPrimary = forwardRef<HTMLButtonElement | HTMLAnchorElement, TInputButtonPrimary>((p, ref) => {
+  const iconName = p.iconLeftName || p.iconRightName
+  let iconPlacement: TInputButton["iconPlacement"] = "labelNotSpecified"
 
-export const InputButtonPrimary = forwardRef<HTMLButtonElement | HTMLAnchorElement, TInputButtonPrimary>((p, ref) => (
-  <InputButton
-    ref={ref}
-    variant="solid"
-    size={p.size}
-    color={EColor.Primary}
-    label={p.label}
-    hug={p.hug}
-    loading={p.loading}
-    disabled={p.disabled}
-  />
-))
+  if (p.iconLeftName) {
+    iconPlacement = "beforeLabel"
+  }
+
+  if (p.iconRightName) {
+    iconPlacement = "afterLabel"
+  }
+
+  return (
+    <InputButton
+      ref={ref}
+      variant="solid"
+      size={p.size}
+      color={EColor.Primary}
+      label={p.label}
+      hug={p.hug}
+      loading={p.loading}
+      disabled={p.disabled}
+      iconName={iconName}
+      iconPlacement={iconPlacement}
+    />
+  )
+})
