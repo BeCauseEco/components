91b5e034b276b35c43985787c8816e99a9140887
Thomas Angelbo Christensen <thomas@eject.io>
2024-12-20 11:30:55 +0100

    wip

--- src/PageBounds/PageBounds.tsx
+++ src/PageBounds/PageBounds.tsx
@@ -1,30 +1,42 @@
 import React, { ReactElement } from "react"
 import styled from "@emotion/styled"
-import { PlaywrightProps } from "@new/Playwright"
 import { StackProps } from "../Stack/Stack"
+import { ComponentBaseProps } from "@new/ComponentBaseProps"
+import { generateErrorClassName, generateErrorStyles, useValidateChildren } from "@new/useValidateChildren"
+import { SpacerProps } from "@new/Stack/Spacer"
+import { DividerProps } from "@new/Divider/Divider"
 
-const Outer = styled.div({
+const Outer = styled.div<Pick<PageBoundsProps, "validateChildrenErrorStyles">>(p => ({
   display: "flex",
   flexDirection: "column",
   width: "100%",
   justifyContent: "flex-start",
   alignItems: "flex-start",
-})
+
+  ...p.validateChildrenErrorStyles,
+}))
 
 const Inner = styled(Outer)({
   maxWidth: "1280px",
   padding: "0 calc(var(--BU) * 4)",
 })
 
-export type TPageBounds = PlaywrightProps & {
-  children: ReactElement<StackProps> | ReactElement<StackProps>[]
+export type PageBoundsProps = ComponentBaseProps & {
+  children:
+    | ReactElement<StackProps | SpacerProps | DividerProps>
+    | ReactElement<StackProps | SpacerProps | DividerProps>[]
 }
 
-export const PageBounds = ({ children, playwrightTestId }: TPageBounds) => (
-  <Outer
-    className="<PageBounds /> - component-page_bounds component-composition-reset"
-    data-playwright-testid={playwrightTestId}
-  >
-    <Inner className="<PageBounds: children /> - ">{children}</Inner>
-  </Outer>
-)
+export const PageBounds = (p: PageBoundsProps) => {
+  const [invalidChildren] = useValidateChildren("Stack", ["Stack", "Spacer", "Divider"], ["PageBounds"], p.children)
+
+  return (
+    <Outer
+      className={`<PageBounds />${generateErrorClassName(invalidChildren)} component-page_bounds component-composition-reset`}
+      data-playwright-testid={p.playwrightTestId}
+      validateChildrenErrorStyles={generateErrorStyles(invalidChildren)}
+    >
+      <Inner className="<PageBounds: children /> - ">{p.children}</Inner>
+    </Outer>
+  )
+}
