9a47928280960b38ee6c301fcb9e0fe6f541d07f
Thomas Angelbo Christensen <thomas@eject.io>
2025-04-10 12:32:51 +0200

    Update DataTable.tsx

--- src/DataTable/DataTable.tsx
+++ src/DataTable/DataTable.tsx
@@ -256,7 +256,7 @@ export type Column = {
   minWidth?: `${number}${"%"}`
   explodeWidth?: boolean
   preventContentCollapse?: boolean
-  sort?: (sortDirection: SortDirection) => (a: string, b: string) => number
+  sort?: (sortDirection: SortDirection) => (a: any, b: any) => number
   avatar?: (rowData: ICellTextProps["rowData"]) => string | undefined
   link?: (rowData: ICellTextProps["rowData"]) => string | (() => void) | undefined
   tooltip?: ((rowData: ICellTextProps["rowData"]) => ReactElement<AlignProps> | string | undefined) | boolean
