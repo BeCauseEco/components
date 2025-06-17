b437b34151655427b333f727b7642c805723f416
Jonas Bruun Jacobsen <jonas@because.eco>
2025-06-17 14:21:08 +0200

    FilteredList always virtualized

--- src/FilteredList/FilteredList.tsx
+++ src/FilteredList/FilteredList.tsx
@@ -1,7 +1,5 @@
 import styled from "@emotion/styled"
-import { Command, CommandEmpty, CommandList } from "cmdk"
 import { useState, useMemo } from "react"
-import { FilteredListItem } from "./FilteredListItem"
 import React from "react"
 import { Text } from "@new/Text/Text"
 import { Color } from "@new/Color"
@@ -10,7 +8,7 @@ import { Spacer } from "@new/Stack/Spacer"
 import { Stack } from "@new/Stack/Stack"
 import { Align } from "@new/Stack/Align"
 import { ComponentBaseProps } from "@new/ComponentBaseProps"
-import { OverflowContainer, OverflowContainerProps } from "@new/OverflowContainer/OverflowContainer"
+import { OverflowContainerProps } from "@new/OverflowContainer/OverflowContainer"
 import { FixedSizeList } from "react-window"
 import { FilteredVirtualListItem } from "@new/FilteredList/FilteredVirtualListItem"
 import { PlaywrightProps } from "@new/Playwright"
@@ -19,11 +17,10 @@ export type ListItemProps = PlaywrightProps & { value: string; label: string; la
 
 export type FilteredListProps = ComponentBaseProps & {
   color: Color
-  maxHeight?: OverflowContainerProps["maxHeight"]
+  maxHeight: OverflowContainerProps["maxHeight"]
   value: string
   disabled?: boolean
   loading?: boolean
-  virtualized?: boolean
   itemHeight?: number
   items: ListItemProps[]
 
@@ -44,14 +41,6 @@ const Container = styled.div({
   },
 })
 
-const CommandEmptyStyled = styled(CommandEmpty)({
-  display: "flex",
-  flexDirection: "column",
-  padding: "calc(var(--BU) * 2) 0",
-  width: "100%",
-  userSelect: "none",
-})
-
 const VirtualizedListContainer = styled.div({
   width: "100%",
   "& [cmdk-item]": {
@@ -64,7 +53,6 @@ export const FilteredList = ({
   id,
   itemHeight,
   color,
-  virtualized,
   "data-playwright-testid": playwrightTestId,
   items,
   value,
@@ -79,97 +67,41 @@ export const FilteredList = ({
     return items?.filter(item => item.label.toLowerCase().includes(filter.toLowerCase())) ?? []
   }, [items, filter])
 
-  if (virtualized) {
-    return (
-      <Container id={id} data-playwright-testid={playwrightTestId} className="<FilteredList /> - ">
-        <Stack vertical data-playwright-testid={playwrightTestId}>
-          <Align vertical topLeft>
-            <InputTextSingle
-              size="large"
-              width="auto"
-              color={color}
-              value={filter}
-              onChange={value => setFilter(value)}
-            />
-
-            <Spacer xsmall />
-            {filteredItems?.length > 0 ? (
-              <VirtualizedListContainer>
-                <FixedSizeList
-                  height={Math.min(maxHeightAsNumber, filteredItems.length * itemHeightAsNumber)}
-                  itemCount={filteredItems.length}
-                  itemSize={itemHeightAsNumber}
-                  itemData={{
-                    items: filteredItems,
-                    selectedValue: value,
-                    onChange: onChange,
-                    color: color,
-                  }}
-                  width="100%"
-                >
-                  {FilteredVirtualListItem}
-                </FixedSizeList>
-              </VirtualizedListContainer>
-            ) : (
-              <Text fill={[color, 700]} small>
-                Nothing found
-              </Text>
-            )}
-          </Align>
-        </Stack>
-      </Container>
-    )
-  }
-
   return (
     <Container id={id} data-playwright-testid={playwrightTestId} className="<FilteredList /> - ">
       <Stack vertical data-playwright-testid={playwrightTestId}>
         <Align vertical topLeft>
-          <Command loop>
-            <InputTextSingle
-              size="large"
-              width="auto"
-              value={filter}
-              color={color}
-              onChange={value => setFilter(value)}
-            />
+          <InputTextSingle
+            size="large"
+            width="auto"
+            color={color}
+            value={filter}
+            onChange={value => setFilter(value)}
+          />
 
-            <Spacer xsmall />
-
-            <CommandList>
-              <CommandEmptyStyled>
-                <Text fill={[color, 700]} small>
-                  Nothing found
-                </Text>
-              </CommandEmptyStyled>
-
-              <Stack vertical hug>
-                <Align vertical topLeft>
-                  <OverflowContainer
-                    axes="vertical"
-                    colorBackground={[Color.White]}
-                    colorForeground={Color.Neutral}
-                    maxHeight={maxHeight}
-                    hug
-                  >
-                    {filteredItems?.map((item, i) => (
-                      <FilteredListItem
-                        key={`filtered-list-item-${i}`}
-                        value={item.value}
-                        label={item.label}
-                        onChange={onChange}
-                        color={color}
-                        src={item.avatarSrc}
-                        title={item.label}
-                        data-playwright-testid={item["data-playwright-testid"]}
-                        size={"large"}
-                      />
-                    ))}
-                  </OverflowContainer>
-                </Align>
-              </Stack>
-            </CommandList>
-          </Command>
+          <Spacer xsmall />
+          {filteredItems?.length > 0 ? (
+            <VirtualizedListContainer>
+              <FixedSizeList
+                height={Math.min(maxHeightAsNumber, filteredItems.length * itemHeightAsNumber)}
+                itemCount={filteredItems.length}
+                itemSize={itemHeightAsNumber}
+                itemData={{
+                  items: filteredItems,
+                  selectedValue: value,
+                  onChange: onChange,
+                  color: color,
+                }}
+                width="100%"
+              >
+                {FilteredVirtualListItem}
+              </FixedSizeList>
+            </VirtualizedListContainer>
+          ) : (
+            <Text fill={[color, 700]} small>
+              Nothing found
+            </Text>
+          )}
         </Align>
       </Stack>
     </Container>
