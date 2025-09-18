41baf60058d6f614b1b3ca44bd96ce04a932648b
Jonas Bruun Jacobsen <jonas@because.eco>
2025-09-18 21:51:08 +0200

    prettier

--- src/InputText/InputNumberSingle.tsx
+++ src/InputText/InputNumberSingle.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useRef, useEffect } from "react"
+import { forwardRef } from "react"
 import { InputTextProps, InputText } from "@new/InputText/internal/InputText"
 import { Color, ColorWithLightness } from "@new/Color"
 
